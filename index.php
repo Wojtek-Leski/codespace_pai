95e97f65227185fc8fa47fd368124106e5e246a2
Wojtek-Leski <157110917+Wojtek-Leski@users.noreply.github.com>
2024-10-23 08:15:13 +0000

    ddd

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
-
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-function sayHello($name) {
-	echo "Hello $name!";
-}
-
+	echo "Hello world";
 ?>
-
-<html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
-</html>
